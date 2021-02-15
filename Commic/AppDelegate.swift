df342aec4d63993bca765d08622a585a6db1daaa
Vitor Silva <vitor@silva.moe>
2021-02-14 22:24:13 -0300

    Initial Commit

---
+++ Commic/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Commic
+//
+//  Created by Vitor Silva on 14/02/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Commic/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Commic
+//
+//  Created by Vitor Silva on 14/02/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
